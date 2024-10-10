a7ef1fd2da86104fbcfda0310085f85403cc9ce6
p-prime1 <preciousnwaka95@gmail.com>
2024-10-11 00:34:11 +0100

    Added a module doc

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
-
+"""
+    Module contains an add funciton that accepts two floats and returns
+    float
+"""
 def add(a: float, b: float) -> float:
     """Function Adds two float"""
     return a + b
