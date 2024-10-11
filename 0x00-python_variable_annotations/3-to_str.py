0f3c511598af3e4a8c85b755e9b0bcb1e1ca61d0
p-prime1 <preciousnwaka95@gmail.com>
2024-10-11 01:45:37 +0100

    func returns a str rep of a value

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module contains a func that returns a str rep of a value"""
+
+
+def to_str(n: float) -> str:
+    """Func accepts a float and returns a strign representation of the value"""
+    return str(n)
