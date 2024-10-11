c99880b66234b049354ddd0d0f86d82f3540478f
p-prime1 <preciousnwaka95@gmail.com>
2024-10-11 02:17:42 +0100

    Func returns tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from typing import Tuple, Union
+"""Contains a func that returns a tuple of it arguments"""
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    a = k, v
+    return a
