d6c1935d8b2d7d3b24c045dd8bc6a90f3858670d
p-prime1 <preciousnwaka95@gmail.com>
2024-10-11 02:01:35 +0100

    imported the typing module

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
+from typing import List
 """Module contains a func that adds list of floats"""
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """Function accepts list of float and returns the sum of them (float)"""
     a: float = 0
     for i in input_list:
