6d0be59afbb3122336ed280d675c2e5f76cb6d0e
p-prime1 <preciousnwaka95@gmail.com>
2024-10-11 01:32:03 +0100

    Fixed pycodestyle errors

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,6 +3,8 @@
     Module contains an add funciton that accepts two floats and returns
     float
 """
+
+
 def add(a: float, b: float) -> float:
     """Function Adds two float"""
     return a + b
