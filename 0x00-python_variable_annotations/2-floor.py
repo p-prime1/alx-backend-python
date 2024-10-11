29d06ce7bcb8c13aff331c1ce4c9089bf3f44570
p-prime1 <preciousnwaka95@gmail.com>
2024-10-11 01:40:26 +0100

    func floors a number

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""Module contains fucn that floors a float"""
+
+
+def floor(n: float) -> int:
+    """Funcion accepts a float, rounds it up and returns an int"""
+    return int(n)
