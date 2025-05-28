1a75fbbdb2f52f547efefbcf02c6183961af7cf1
p-prime1 <preciousnwaka95@gmail.com>
2025-05-28 13:15:20 +0100

    Simulated fetching paginated data from a user database using a lazy generator function

---
+++ python-generators-0x00/2-lazy_paginate.py
@@ -0,0 +1,17 @@
+import sqlite3
+
+def paginate_users(page_size, offset):
+
+    with sqlite3.connect('ALX_prodev') as conn:
+        cursor = conn.cursor()
+        result = cursor.execute('SELECT * FROM user_data LIMIT ? OFFSET ?', (page_size, offset))
+    return result
+
+def lazy_paginate(page_size):
+    offset = 0
+    while True:
+        result = paginate_users(page_size, offset)
+        if not result:
+            break
+        yield result
+        offset += page_size
