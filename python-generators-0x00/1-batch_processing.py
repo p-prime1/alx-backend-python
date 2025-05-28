77457b1c21fb3b585a04e677c264ed5945efd40f
p-prime1 <preciousnwaka95@gmail.com>
2025-05-28 11:43:10 +0100

    Code fetches and process data in batches

---
+++ python-generators-0x00/1-batch_processing.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+def stream_users_in_batches(batch_size):
+    i = 0
+    with sqlite3('ALX_prodev.db') as conn:
+        cursor = conn.cursor()
+        res = cursor.execute('SELECT * FROM user_data')
+        while i < batch size:
+            for i in res:
+                yield i
+            i += 1
+
+def batch_processing(batch_size):
+    i = 0
+    with sqlite3('ALX_prodev.db') as conn:
+        cursor = conn.cursor()
+        result = cursor.execute('SELECT * FROM user_data WHILE age > 25')
+        while i < batch_size:
+            for i in result:
+                yield i
+            i += 1
