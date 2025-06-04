cc3d6a6b798f33ce0efea5d7bd2408892d640537
p-prime1 <preciousnwaka95@gmail.com>
2025-06-04 12:21:36 +0100

    Callled the function with the args and kwargs argument

--- python-decorators-0x01/0-log_queries.py
+++ python-decorators-0x01/0-log_queries.py
@@ -2,5 +2,5 @@ from datetime import datetime, connect
 def log_queries(func):
     def wrapper(*args, **kwargs):
         print(args, datetime.date())
-        func()
+        func(*args, **kwargs)
     return wrapper       
