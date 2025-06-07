dfdddfe0dcc5b3a34e961c1d217fb95c7146fad1
p-prime1 <preciousnwaka95@gmail.com>
2025-06-07 22:41:51 +0100

    New class test the utils.get_json function

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -5,9 +5,10 @@ Includes unit tests with parameterized inputs.
 """
 
 
-from utils import access_nested_map
+from utils import access_nested_map, get_json
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -41,3 +42,27 @@ class TestAccessNestedMap(unittest.TestCase):
         Test that the access_nested_map raises the expected exceptions"""
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """
+    Unit Test for testing the get_json function in the
+    utils model
+    """
+
+    @parameterized.expand(
+        [
+            ("http://example.com", {"payload": True}),
+            ("http://holberton.io", {"payload": False}),
+        ]
+    )
+    @patch("utils.requests.get")
+    def test_get_json(self, url, payload, mock_get):
+        """
+        Test that the get_json returns the expected result
+        """
+        mock_get.return_value.json.return_value = payload
+
+        result = get_json(url)
+        self.assertEqual(result, payload)
+        mock_get.assert_called_once_with(url)
