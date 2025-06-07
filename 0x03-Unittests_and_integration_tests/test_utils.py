345be0f493461f90b78fe6802776c0a4ceb6a505
p-prime1 <preciousnwaka95@gmail.com>
2025-06-07 22:12:24 +0100

    Added a new method that test a KeyError is raised

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -16,14 +16,28 @@ class TestAccessNestedMap(unittest.TestCase):
     from the utils module.
     """
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(self, nested_map, path, result):
         """
         Test that access_nested_map returns the expected result
         for various nested structures and paths.
         """
         self.assertEqual(access_nested_map(nested_map, path), result)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path, result):
+        """
+        Test that the access_nested_map raises the expected exceptions"""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
