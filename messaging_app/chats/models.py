c9b2028c77f96131387bdcc335cbc8ac09bf3744
p-prime1 <preciousnwaka95@gmail.com>
2025-06-11 22:32:51 +0100

    Design models for users messages and conversations

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class user(AbstractUser):
+    """Model for the user"""
+
+    Name = models.CharField(max_length=200)
+    user_name = models.CharField(max_length=200)
+    profile_picture - models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user_name
+
+
+class conversation:
+    """Model tracks which users are involved in a conversation"""
+
+    users = models.ManyToManyField(User, related_name="conversations")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation ID: {self.id}"
+
+
+class message:
+    """Model containing sender and conversation"""
+
+    sender = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="sent_messages"
+    )
+    recipient = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="received_messages",
+    )
+    body = models.TextField()
