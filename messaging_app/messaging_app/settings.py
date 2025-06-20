86cb5f7de903620155caf475969d8b83d60b259d
p-prime1 <preciousnwaka95@gmail.com>
2025-06-20 19:24:51 +0100

    Used  IsAuthenticated to ensure all users must be logged in

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -73,7 +73,7 @@ WSGI_APPLICATION = "messaging_app.wsgi.application"
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES':[
         'rest_framework.authentication.SessionAuthentication',
