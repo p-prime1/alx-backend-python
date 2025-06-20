861bbcf43e23acdf936e7478191277b1f66e23bc
p-prime1 <preciousnwaka95@gmail.com>
2025-06-20 20:26:28 +0100

    Added nested relationships

--- messaging_app/chats/serializers.py
+++ messaging_app/chats/serializers.py
@@ -1,11 +1,17 @@
 form rest_framework import serializers
 from .models import User, Conversation, Message
+from rest_framework.serializers import CharField, SerializerMethodField, ValidationError
 
 
 class UserSerializer(serializers.ModelSerializer):
+    full_name = serializers.SerializerMethodField()
+    
     class Meta:
         model = User
         fields = '__all__'
+
+    def get_full_name(self, obj):
+        return f"{obj.first_name} {obj.last_name}"
 
 
 class MessageSerializer(serializers.ModelSerializer):
@@ -14,6 +20,11 @@ class MessageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Message
         fields = '__all__'
+
+    def validate_message_body(self, value):
+        if 'badword' in value.lower():
+            raise serializers.ValidationError("Inappropriate content detected.")
+        return value
 
 
 class ConversationSerializer(serializers.ModelSerializer):
