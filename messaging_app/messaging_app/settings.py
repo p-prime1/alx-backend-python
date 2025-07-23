da2f575c942b617a0b0f6900d97f940a29ed9897
p-prime1 <preciousnwaka95@gmail.com>
2025-07-23 22:00:35 +0100

    add permission and auth files

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     "rest_framework",
     "chats",
+    "rest_framework_simplejwt"
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
